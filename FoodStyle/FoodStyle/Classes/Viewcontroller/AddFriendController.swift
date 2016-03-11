442bf502b0c056266185f5ced6ce22a1b641cb60
Wong <stylew@163.com>
2016-03-12 00:11:14 +0800

    sectionHead_added

--- FoodStyle/FoodStyle/Classes/Viewcontroller/AddFriendController.swift
+++ FoodStyle/FoodStyle/Classes/Viewcontroller/AddFriendController.swift
@@ -13,10 +13,10 @@ class AddFriendController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "添加好友"
-        tableView = UITableView(frame: CGRectZero, style: UITableViewStyle.Grouped)
+        tableView = UITableView(frame: CGRectZero, style: UITableViewStyle.Plain)
         tableView.sectionHeaderHeight = 15
         tableView.sectionFooterHeight = 0
-        tableView.contentInset = UIEdgeInsetsMake(-20, 0, 0, 0)
+//        tableView.contentInset = UIEdgeInsetsMake(-20, 0, 0, 0)
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "取消", style: UIBarButtonItemStyle.Plain, target: self, action: "cancel")
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "map_icon_zoom_out"), style: UIBarButtonItemStyle.Plain, target: self, action: "searchFriend")
     }
@@ -31,53 +31,6 @@ class AddFriendController: UITableViewController {
     }
 
 }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 
 extension AddFriendController{
 
@@ -111,6 +64,7 @@ extension AddFriendController{
 
     override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headLabel = UILabel(frame: CGRectMake(0,0,SCREEN_RECT().width,15))
+        headLabel.backgroundColor = UIColor.lightGrayColor()
         switch section{
         case 0:
             headLabel.text = "邀请好友"
